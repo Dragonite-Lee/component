13c1ed228a223e45482334840b9444065bc4a99a
dragonite-lee <ross1222@naver.com>
2022-12-05 21:44:00 +0900

    slide메뉴의 쉐브론 애니메이션화 및 z-index조절

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,28 @@
 import styles from "./home.module.css";
 import { Link } from 'react-router-dom';
 import chevronImg from '../chevron-up.png'
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import transition from '../transition.PNG'
 
 export default function Home() {
-    let urlList = [transition,1,2];
-    const [url, setUrl] = useState(urlList);
-
+    let urlList = [transition];
+    let dataList = ["transition"]
     
+    const [click, setClick] = useState(false);
     const chevronBtn = useRef(null);
+    
     
     function onclickHandler(e) {
         chevronBtn.current.style.transform = "translateY(-100vh)"
         chevronBtn.current.style.transition = "all 2s"
     }
+    useEffect(()=>{
+        if (click === true) {
+            onclickHandler()
+        }
+    },[click])
 
+    console.log(click)
     return (
         <div className={styles.container} >
             <div className={styles.slide_menu} ref={chevronBtn} >
@@ -29,7 +36,7 @@ export default function Home() {
                     className={styles.chevron_up}
                     src={chevronImg}
                     alt="쉐브론" 
-                    onClick={(e)=>onclickHandler()}
+                    onClick={()=>setClick(true)}
                 />
             </div>
             <div className={styles.content}>
@@ -39,14 +46,14 @@ export default function Home() {
                 <div className={styles.main_container}>
                     {urlList.map((data,index)=>(
                         <div key={index} className={styles.item}>
-                            <Link to={`/component/${data}`} >
+                            <Link to={`/component/${dataList[index]}`} >
                                 <img 
                                     src={data} 
                                     alt="컴포넌트사진"
                                     className={styles.img}
                                 />
                             </Link>
-                            <div className={styles.text}>{data}</div>
+                            <div className={styles.text}>{dataList[index]}</div>
                         </div>
                     ))}
                 </div>
